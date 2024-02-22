e73a9a7d856b647b487d5cdfb71040ebf07397a6
KoDiian <tristan.bert.12@gmail.com>
2024-02-22 14:40:08 +0100

    Creation de la class pacman

--- base_travail_5.py
+++ base_travail_5.py
@@ -4,6 +4,7 @@ import random
 from labyrinthe import Labyrinthe
 from grid import Grid
 from utils import Pos
+from pac import *
 # pygame setup
 pygame.init()
 
---
+++ pac.py
@@ -0,0 +1,17 @@
+import pygame
+class Pacman:
+    def __init__(self):
+        self.body = [(200, 200)]
+        self.direction = (1, 0)
+
+    def move(self, tilesize, size):
+        width = size[0]
+        height = size[1]
+        x, y = self.body[0]
+        dx, dy = self.direction
+        new_head = ((x + dx * tilesize) % width, (y + dy * tilesize) % height)
+        self.body.insert(0, new_head)
+    
+    def draw(self, surface, tilesize):
+        for segment in self.body:
+            pygame.draw.rect(surface, (0, 255,0), (segment[0], segment[1], tilesize, tilesize))
